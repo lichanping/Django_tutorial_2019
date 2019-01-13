36751da5095de236994832b007f4bb064065a751
jixin <lichanping@hotmail.com>
2019-01-13 16:43:19 +0800

    add static directory

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from polls.models import Choice
+from .models import Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':
+                              ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
