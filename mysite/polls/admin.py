45a42549fb5c2213f61435896abfeb607e98e62d
jixin <lichanping@hotmail.com>
2019-01-15 22:19:29 +0800

    1.resolve django low version in requirements doc 2.update question model to show custermized column in admin site 3.inherit the django templates with minor changes locally.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,12 +4,15 @@ from polls.models import Choice
 from .models import Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes':
